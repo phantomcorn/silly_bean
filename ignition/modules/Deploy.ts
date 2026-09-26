68edf2fde64d2ded610eb328a5fa2d4f2ddbdd22
phantomcorn <jj.phantakorn@outlook.com>
2026-09-26 22:34:16 +0700

    fix: bean contract must be owned by farm to mint + deploy change

--- ignition/modules/Deploy.ts
+++ ignition/modules/Deploy.ts
@@ -2,7 +2,9 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
 export default buildModule("DiscreteBean", (m) => {
   const bean = m.contract("DiscreteBean");
-  const farm = m.contract("RegularFarm", [bean]) 
+  const farm = m.contract("RegularFarm", [bean]);
+
+  m.call(bean, "transferOwnership", [farm]);
 
   return { bean, farm };
 });
