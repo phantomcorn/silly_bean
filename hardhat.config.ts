e30bbc3d2bc4c4d05ec18ef03b234eb210c1b2de
phantomcorn <jj.phantakorn@outlook.com>
2026-09-26 22:51:56 +0700

    add etherscan key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -36,4 +36,9 @@ export default defineConfig({
       accounts: [configVariable("SEPOLIA_PRIVATE_KEY")],
     },
   },
+  verify: {
+    etherscan: {
+      apiKey: configVariable("ETHERSCAN_API_KEY"),
+    },
+  },
 });
